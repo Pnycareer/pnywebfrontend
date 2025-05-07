55b0802f0ef7b1febc5122a57b2fa265a7311f76
rahilazhar12 <rahil.azhar1012@gmail.com>
2025-05-07 10:50:02 +0500

    first commit

--- src/components/Meta/Metadata.js
+++ src/components/Meta/Metadata.js
@@ -9,13 +9,14 @@ const Metadata = ({
 }) => {
   const metaTitle = title || "Default Title";
   const metaDescription = description || "Default description for the page.";
+  const metaImage = image
+    ? `http://api.pnytrainings.com/${image}`
+    : "/default-image.jpg";
   const metaUrl = url || process.env.NEXT_PUBLIC_SITE_URL;
   const metaRobots = noindex ? "noindex, nofollow" : "index, follow";
   const metaCanonical = canonicalUrl || metaUrl;
-  const metaSiteName = siteName || process.env.NEXT_PUBLIC_SITE_URL;
-  const metaImage = image
-    ? `${process.env.NEXT_PUBLIC_API_URL.replace(/\/$/, "")}/${image.replace(/^\//, "")}`
-    : `https://images.pexels.com/photos/56866/garden-rose-red-pink-56866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2`;
+  const metaSiteName = siteName || "https://pnywebfrontend.vercel.app";
+
 
   return (
     <>
@@ -23,17 +24,13 @@ const Metadata = ({
       <meta name="description" content={metaDescription} />
       <meta name="robots" content={metaRobots} />
       <link rel="canonical" href={metaCanonical} />
-      
       {/* Open Graph / Facebook / WhatsApp */}
       <meta property="og:type" content="website" />
       <meta property="og:title" content={metaTitle} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:image" content={metaImage} />
-      <meta property="og:image:width" content="1200" />
-      <meta property="og:image:height" content="630" />
       <meta property="og:url" content={metaUrl} />
       <meta property="og:site_name" content={metaSiteName} />
-      
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={metaTitle} />
