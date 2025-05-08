038f3b017ef460e776932ff0a849d0a334822c61
rahilazhar12 <rahil.azhar1012@gmail.com>
2025-05-09 00:22:38 +0500

    first commit

--- src/components/Animation/Animation.js
+++ src/components/Animation/Animation.js
@@ -1,37 +1,14 @@
 'use client'
-import { DotLottieReact } from "@lottiefiles/dotlottie-react";
-import animation1 from '@/components/Animation/robot.json'
+import { Player } from "@lottiefiles/react-lottie-player";
+import robotAnimation from '@/components/Animation/robot.json';
 
 const Animation = () => {
   return (
-    <>
-      <div className="grid grid-cols-3">
-        <div>
-        <DotLottieReact
-           animationData={animation1}
-          loop
-          autoplay
-        />
-        </div>
-        {/* <div>
-        <DotLottieReact
-          src="https://lottie.host/9e015b22-1b77-457b-88ab-51dfea1e736a/81Lv7TNBFW.lottie"
-          loop
-          autoplay
-        />
-        </div> */}
-        {/* <div>
-        <DotLottieReact
-          src="https://lottie.host/0237c7cf-ffc2-418e-bee0-7d4fbbc00970/4DM7dCeC9B.lottie"
-          loop
-          autoplay
-        />
-        </div> */}
-        <div>
-        
-        </div>
+    <div className="grid grid-cols-3">
+      <div>
+      <iframe src="https://lottie.host/embed/75622fa9-d0d8-493c-9b7c-d1d07a086341/NVLW1u7YA5.lottie"></iframe>
       </div>
-    </>
+    </div>
   );
 };
 
