8a505afb03c653a0ef7becbbeb8a14d5339982c0
rahilazhar12 <rahil.azhar1012@gmail.com>
2025-05-30 14:14:21 +0500

    first commit

--- src/app/(pages)/about/page.js
+++ src/app/(pages)/about/page.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import About from './About'
+import React from "react";
+import About from "./About";
+import Metadata from "@/components/Meta/Metadata";
 
-const page = () => {
+const Page = () => {
   return (
     <>
-    <About/>
+      <Metadata
+        title="About Us | NextCMS"
+        description="Learn more about our mission, team, and what makes NextCMS stand out."
+        url="https://nextcms.shop/about"
+        // image="images/about-banner.jpg" // Optional: relative to your base URL
+        canonicalUrl="https://nextcms.shop/about"
+      />
+      <About />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
