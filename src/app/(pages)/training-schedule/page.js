16e87ea71e05713a4bce58f047566d8204c00219
rahilazhar12 <rahil.azhar1012@gmail.com>
2025-06-13 09:53:54 +0500

    first commit

--- src/app/(pages)/training-schedule/page.js
+++ src/app/(pages)/training-schedule/page.js
@@ -1,36 +1,50 @@
+// import TrainingSchedule from "./Trainingschedule";
+
+// // Fetch training data
+// const getTrainings = async () => {
+//   const res = await fetch(
+//     "https://lms.pnytraining.com/api/trainingSchedules?type=month&duration=1",
+//     { next: { revalidate: 3600 } }
+//   );
+//   const data = await res.json();
+//   return data?.Batches || {};
+// };
+
+// // Fetch meta info
+// const getMeta = async () => {
+//   const res = await fetch(
+//     "https://www.admin777.pny-trainings.com/api/metas/training-schedule",
+//     { next: { revalidate: 3600 } }
+//   );
+//   const data = await res.json();
+//   return data?.metas?.[0] || {}; // get the first meta entry
+// };
+
+// const Page = async () => {
+//   const [allBatches, meta] = await Promise.all([getTrainings(), getMeta()]);
+
+//   return (
+//     <div>
+//       <title>{meta.meta_title}</title>
+//       <meta name="description" content={meta.meta_description} />
+//       <meta name="robots" content="noindex, nofollow" />
+//       <TrainingSchedule batches={allBatches} />
+//     </div>
+//   );
+// };
+
+// export default Page;
+
+import React from 'react'
 import TrainingSchedule from "./Trainingschedule";
 
-// Fetch training data
-const getTrainings = async () => {
-  const res = await fetch(
-    "https://lms.pnytraining.com/api/trainingSchedules?type=month&duration=1",
-    { next: { revalidate: 3600 } }
-  );
-  const data = await res.json();
-  return data?.Batches || {};
-};
-
-// Fetch meta info
-const getMeta = async () => {
-  const res = await fetch(
-    "https://www.admin777.pny-trainings.com/api/metas/training-schedule",
-    { next: { revalidate: 3600 } }
-  );
-  const data = await res.json();
-  return data?.metas?.[0] || {}; // get the first meta entry
-};
-
-const Page = async () => {
-  const [allBatches, meta] = await Promise.all([getTrainings(), getMeta()]);
-
+const page = () => {
   return (
     <div>
-      <title>{meta.meta_title}</title>
-      <meta name="description" content={meta.meta_description} />
-      <meta name="robots" content="noindex, nofollow" />
-      <TrainingSchedule batches={allBatches} />
+      <TrainingSchedule/>
     </div>
-  );
-};
+  )
+}
 
-export default Page;
+export default page
+
