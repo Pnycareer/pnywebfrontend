336c056859771b55f79abdf32907982c6ba7b736
rahilazhar12 <rahil.azhar1012@gmail.com>
2025-09-02 00:30:54 +0500

    first commit

--- src/app/(pages)/blog/loading.js
+++ src/app/(pages)/blog/loading.js
@@ -1,7 +1,7 @@
 // app/[slug]/loading.jsx
 
 import React from 'react';
-import Loader from '@/components/loader/Loader'; // You already have this component
+import Loader from '@/components/loader/Loader'; // You already have this component.
 
 const Loading = () => {
   return (
