5628072208bc23d9925c89b6bba73526b77a5236
rahilazhar12 <rahil.azhar1012@gmail.com>
2025-09-09 13:50:32 +0500

    first commit

--- src/app/(pages)/city/[slug]/page.js
+++ src/app/(pages)/city/[slug]/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { notFound } from "next/navigation";
 import CityCoursescategory from "./CityCoursescategory";
 import axiosInstance from "@/utils/axiosInstance";
+import Metadata from "@/components/Meta/Metadata";
 
 const page = async ({ params }) => {
   const { slug } = params;
@@ -12,15 +13,30 @@ const page = async ({ params }) => {
     const data = response.data;
 
     const metadata = {
-      metatitle: data?.category_Name || "Course Not Found",
-      metadescription:
-        data?.category_Description || "This course does not exist.",
+      metatitle: data?.category_Meta_Title?.trim()
+        ? data.category_Meta_Title
+        : data?.category_Name || "",
+
+      metadescription: data?.category_Meta_Description?.trim()
+        ? data.category_Meta_Description
+        : data?.category_Description || "",
+
+      canonical: `https://www.pnytrainings.com/${
+        data?.category_Meta_Title || data?.category_Name || ""
+      }`,
     };
 
     return (
       <>
-        <title>{metadata.metatitle}</title>
-        <meta name="description" content={metadata.metadescription} />
+        <Metadata
+          title={metadata.metatitle}
+          description={metadata.metadescription}
+          canonicalUrl={`https://www.pnytrainings.com/${metadata.metatitle}`}
+          url={`https://www.pnytrainings.com/${metadata.metatitle}`}
+          image={metadata.image}
+          siteName="Pnytrainings"
+        />
+
         <CityCoursescategory slug={slug} subcategory={data} />
       </>
     );
